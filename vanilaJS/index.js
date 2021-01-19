486441526c736051387abd2a2ae8f4ee26c37304
son-of-sokcho <gurwo1798@gmail.com>
2021-01-19 17:46:55 +0900

    madeVanilaJS

---
+++ vanilaJS/index.js
@@ -0,0 +1,21 @@
+const title = document.querySelector("#title");
+title.innerHTML = "Click here!";
+title.style.color = "white";
+document.title = "I own you now";
+
+function HandClick(){
+    title.style.color = "blue";
+    title.innerHTML = "Now, fuck off";
+}
+
+title.addEventListener("click",HandClick);
+
+if ( 10 = "10" ){
+    console.log("hi");
+}
+else if ( 10 = 10 ){
+    console.log("h0");
+}
+else {
+    console.log("ahahhahaha");
+}
